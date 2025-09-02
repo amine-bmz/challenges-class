0f718d4555d299cd8de2a5fd896c439c2a29d41e
amine-bmz <electrofatal62@gmail.com>
2025-09-02 19:26:39 +0200

    Update code

--- projet 5/projet 5/main.c
+++ projet 5/projet 5/main.c
@@ -3,6 +3,21 @@
 
 int main()
 {
-    printf("Hello world!\n");
+float t;
+printf("entrez la Température!\n");
+ scanf("%f" , t);
+ if (t < 0){
+    printf("solide");
+    scanf("%f" , t);
+}
+ else if (0 <= t < 100){
+    printf("Liquide");
+    scanf("%f" , t);
+ }
+ else if(t >= 100){
+    printf("Gaz");
+    scanf("%f" , t);
+ }
+
     return 0;
 }
