c9df7bcba8c60a7a8bce66b37cc6585e63732190
amine-bmz <electrofatal62@gmail.com>
2025-09-08 17:32:59 +0100

    first-commit

--- tableau-challenge-7/tableau-challenge-7/main.c
+++ tableau-challenge-7/tableau-challenge-7/main.c
@@ -3,6 +3,19 @@
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
-}
+    int n;
+
+    printf("Entrez le nombre d'elements : ");
+    scanf("%d", &n);
+
+    int tableau[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("donner l'element %d :", i + 1);
+        scanf("%d", &tableau[i]);
+    }
+    for(int i = 0; i < )
+        return 0;
+    }
+
