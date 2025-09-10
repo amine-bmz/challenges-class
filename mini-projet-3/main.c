dca096126c7080096deba3946c64032e214b9e36
amine-bmz <electrofatal62@gmail.com>
2025-09-10 10:33:37 +0100

    commit

---
+++ mini-projet-3/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+    char *posword[] = {hapy, good, Beautiful};
+    char *posrepo[]= {
+        oh !thats good !!,
+        Hi !Great to hear that !};
+        
+    char *negword[] = {sad, bad};
+    char *negrepo[] =
+        {
+            Oh no !Hope things get better soon,
+            its fine !Hope things get better soon};
+
+    char *nullwords[] ={hello, bey};
+    char *nullrepo[] = {hi, how ara you,
+                        I see you soon}
+
+int main()
+{
+    int a;`
+    printf("Bot : entrer votre words :");
+    printf("me :");
+    scanf("%s", a);
+
+    if ()
+    {
+        
+    }
+    
+    return 0;
+}
