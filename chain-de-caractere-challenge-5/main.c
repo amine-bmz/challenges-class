24bbb6300c2251a3546c27b5c62a743293e79311
amine-bmz <electrofatal62@gmail.com>
2025-09-11 09:44:27 +0100

    commit

--- chain-de-caractere-challenge-5/main.c
+++ chain-de-caractere-challenge-5/main.c
@@ -4,12 +4,28 @@
 int main(){
 
   char word[40];
-   int n = strlen(words);
-   for (int i = 0; i < n / 2 - 1; i++)
+   int n, j; 
+   char tmp;
+
+   printf("entre un words :");
+   fgets(word, sizeof(word), stdin);
+   int sup = strlen(word);
+   if (word[sup -1] == '\n')
    {
-    
+    word[sup - 1] = '\0';
+    sup--;
    }
    
-  
+   n = 0;
+   j = sup - 1;
+   while (n < j)
+   {
+     tmp = word[n];
+     word[n] = word[j];
+     word[j] = tmp;
+     n++;
+     j--;
+   }
+  printf("the word inverse est :%s", word);
     return 0;
 }
